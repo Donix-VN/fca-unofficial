0304e1d7203f7f169db56c4a366e3f5780576c49
LunarKrystal <khoi0603k@gmail.com>
2025-10-06 16:07:30 +0700

    change method check version from github raw to npm

--- func/checkUpdate.js
+++ func/checkUpdate.js
@@ -1,25 +1,11 @@
-const https = require('https');
+const axios = require('axios');
 const logger = require("./logger");
 const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const localPkgPath = path.join(__dirname, "../package.json");
-const githubPkgUrl = 'https://raw.githubusercontent.com/Donix-VN/fca-unofficial/main/package.json';
+const npmPkgUrl = 'https://registry.npmjs.org/@dongdev/fca-unofficial';
 
-function getRemotePackageJson(url, callback) {
-    https.get(url, (res) => {
-        let data = '';
-        res.on('data', chunk => data += chunk);
-        res.on('end', () => {
-            try {
-                const pkg = JSON.parse(data);
-                callback(null, pkg);
-            } catch (err) {
-                callback(err);
-            }
-        });
-    }).on('error', err => callback(err));
-}
 async function checkAndUpdateVersion(callback) {
     const execPromise = (cmd) => new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
@@ -34,25 +20,21 @@ async function checkAndUpdateVersion(callback) {
     }
     const localPkg = JSON.parse(fs.readFileSync(localPkgPath, 'utf8'));
     logger(`Đang kiểm tra phiên bản...`, "info");
-    getRemotePackageJson(githubPkgUrl, async (err, remotePkg) => {
-        if (err) {
-            logger(`Lỗi khi lấy package.json từ Github: ${err}`, "error");
-            return callback(err);
-        }
-        if (localPkg.version !== remotePkg.version) {
-            logger(`Đã có bản mới (${remotePkg.version}) Phiên bản đang dùng (${localPkg.version}). Đang cập nhật...`, "info");
-            fs.writeFileSync(localPkgPath, JSON.stringify(remotePkg, null, 2));
-            logger(`Đã cập nhật package.json lên phiên bản mới: ${remotePkg.version}`, "info");
+    try {
+        const response = await axios.get(npmPkgUrl);
+        const remotePkg = response.data;
+        if (localPkg.version !== remotePkg['dist-tags'].latest) {
+            logger(`Đã có bản mới (${remotePkg['dist-tags'].latest}) Phiên bản đang dùng (${localPkg.version}). Đang cập nhật...`, "info");
             try {
                 const { stderr } = await execPromise('npm i @dongdev/fca-unofficial@latest');
-                logger(`Đã cập nhật fca lên phiên bản mới nhất: ${remotePkg.version}, khởi động lại để áp dụng.`, "info");
+                logger(`Đã cập nhật fca lên phiên bản mới nhất: ${remotePkg['dist-tags'].latest}, khởi động lại để áp dụng.`, "info");
                 if (stderr) logger(stderr, "error");
                 callback(null);
             } catch (e) {
                 logger(`Lỗi khi chạy npm install: ${e.error}. Đang thử tải về từ Github...`, "error");
                 try {
                     const { stderr } = await execPromise('npm i https://github.com/DongDev-VN/fca-unofficial');
-                    logger(`Đã tải về từ Github thành công: ${remotePkg.version}`, "info");
+                    logger(`Đã tải về từ Github thành công!`, "info");
                     if (stderr) logger(stderr, "error");
                     callback(null);
                 } catch (gitErr) {
@@ -64,7 +46,10 @@ async function checkAndUpdateVersion(callback) {
             logger('Phiên bản đang dùng đã là mới nhất.', "info");
             callback(null);
         }
-    });
+    } catch (error) {
+        logger(`Lỗi khi kiểm tra và cập nhật phiên bản: ${error}`, "error");
+        callback(error);
+    }
 }
 
 module.exports.checkAndUpdateVersion = checkAndUpdateVersion;
